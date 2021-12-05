1cf8442cf46de8fd25d50a82e183abeb6662d90c
KuuShyn <49732964+KenShynn@users.noreply.github.com>
2021-12-05 12:05:18 +0800

    final

--- Exercise#6 - Sum and Average of numbers using loops and selection statements.py
+++ Exercise#6 - Sum and Average of numbers using loops and selection statements.py
@@ -1,18 +1,18 @@
-# tSum = 0
-# tAvg = 0
-# count = 0
+tSum = 0
+tAvg = 0
+count = 0
 
-# while True: 
-#     numin = input("Enter a number or just enter to quit: ")
-#     count += 1    #counts the user's inputs
+while True: 
+    numin = input("Enter a number or just enter to quit: ")
+    count += 1    #counts the user's inputs
     
-#     if numin == "":        
-#         break   #breaks the loop if there's no input
+    if numin == "":        
+        break   #breaks the loop if there's no input
          
-#     tSum += float(numin)    #converts the numbers string to float and adds the numbers every input
-#     tAvg = tSum / count     #divides the total sum with number of inputs 
+    tSum += float(numin)    #converts the numbers string to float and adds the numbers every input
+    tAvg = tSum / count     #divides the total sum with number of inputs 
 
-# print("The sum is", tSum)
-# print("The average is", tAvg)
-#King Aj Magalona
+print("The sum is", tSum)
+print("The average is", tAvg)
+# King Aj Magalona
 
