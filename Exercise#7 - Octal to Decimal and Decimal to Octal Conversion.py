7092642dada402735a44aac788d1153c5733632a
KenShynn <magalonakaj@gmail.com>
2021-12-05 12:00:11 +0800

    final

--- Exercise#7 - Octal to Decimal and Decimal to Octal Conversion.py
+++ Exercise#7 - Octal to Decimal and Decimal to Octal Conversion.py
@@ -4,34 +4,36 @@ decimal = int(input("Enter a decimal Integer: "))
 if decimal == 0:
     print(0)
 else:
-    print("Quotient Remainder Octal")
-    
+    print("Quotient Remainder Octal")   
+
 octalString = " "
 
 while decimal > 0:
     remainder = decimal % 8
     decimal = decimal // 8
     octalString = str(remainder) + octalString
-    print("%5d%8d%12s" % (decimal, remainder, octalString))   
+    print('%5d%8d%12s' % (decimal, remainder, octalString))   
 print("The octal representation is", octalString)
-# Or a better way
+
+# err.... ye solution hehe
 # decToOct = int(input('Enter a decimal Integer: '))
 # print(oct(decToOct).replace('0o', ""))
-
-
 
 #===================================================================#
 
 #Octal to Decimal
-octal = input("\nEnter a string of octal digits: ")
-decimalV = 0
-length = len(octal)
-for n in octal:
-    length -= 1
-    decimalV += pow(8, length) * int(n)
-print("The integer value is", decimalV)
+octal = input("Enter a string of octal digits: ")
+deciVal = 0 
+base = 1
+  
+while octal: 
+    digit = int(octal) % 10
+    octal = int(octal) / 10
+    deciVal += digit * base
+    base = base * 8
+print("The decimal value is:", deciVal)
 
-# Or a better way   
+# One line solution hehe   
 # print(int(input('Enter a string of octal digits: '), 8))    
 
 #King Aj Magalona
