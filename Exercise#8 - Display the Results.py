9935ff0ca5f8c8d327b732e0b4586f0bf97ececc
KenShynn <magalonakaj@gmail.com>
2021-12-12 01:05:44 +0800

    kyaa

--- Exercise#8 - Display the Results.py
+++ Exercise#8 - Display the Results.py
@@ -1,7 +1,7 @@
 #King Aj Magalona
 data = [5, 3, 7]
-print("task 1")
 
+print("task 1")
 print(data[2])
 print(data[-1])
 print(len(data))
@@ -11,14 +11,13 @@ print(data + [2, 10, 5])
 print(tuple(data))
 
 print("\ntask 2")
-
 data[0] = -data[0]
 print(data)
 
 data.append(10)
 print(data)
 
-data.insert(2,22)
+data.insert(2, 22)
 print(data)
 
 data.pop(1)
